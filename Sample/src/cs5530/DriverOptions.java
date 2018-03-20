8701be62d940533f6c6c46e16c26fbe48d992323
hinghing15 <huang.kathy.15@gmail.com>
2018-03-19 23:58:02 -0600

    Update UUber driver car option finished

--- Sample/src/cs5530/DriverOptions.java
+++ Sample/src/cs5530/DriverOptions.java
@@ -52,7 +52,10 @@ public class DriverOptions
 		     		addNewCar();
 		       		break;
 		       	case 2: //UUpdate a UUber Car
-		       		updateCar();
+		       		if(printCars())
+		       		{
+		       			updateCar();
+		       		}
 		       		break;
 	       	}
         }
@@ -140,7 +143,7 @@ public class DriverOptions
 		catch (Exception e) { /* ignore close errors */ }
 		return false; // failure to login
 	}
-	public boolean updateCar()
+	public boolean printCars()
 	{
 		String sql;
 		try 
@@ -154,17 +157,19 @@ public class DriverOptions
 				boolean isNext = result.next();
 				if(isNext)
 				{
-				    System.out.println(result.getString("vin") + " " + result.getString("category") + " " +
-				    		result.getString("make") + " " + result.getString("model") + " " + result.getString("year"));
+				    System.out.println("vin: " + result.getString("vin") + "\n\t" + result.getString("category") + ", " +
+				    		result.getString("make") + ", " + result.getString("model") + ", " + result.getString("year"));
 				    while(result.next())
 				    {
-					    System.out.println(result.getString("vin") + " " + result.getString("category") + " " +
-					    		result.getString("make") + " " + result.getString("model") + " " + result.getString("year"));
+					    System.out.println("vin: " + result.getString("vin") + "\n\t" + result.getString("category") + ", " +
+					    		result.getString("make") + ", " + result.getString("model") + ", " + result.getString("year"));
 				    }
+				    System.out.println("\n");
+				    return true;
 				}
 				else
 				{
-					System.out.println("You have no registered carss.\n");
+					System.out.println("You have no registered cars.\n");
 				}
 			} 
 			catch(SQLException e) 
@@ -174,5 +179,137 @@ public class DriverOptions
 		}
 		catch (Exception e) { /* ignore close errors */ }
 		return false; // failure to log in
+	}
+	public boolean updateCar()
+	{
+		try 
+		{
+			String choice = null;
+			int c2 = 0;
+			String category = null;
+			String make = null;
+			String model = null;
+			String yearS = null;
+			String sql=null;
+			System.out.println("Type in the vin of the vehicle you would like to update: ");
+			try 
+			{
+				 while(true)
+				 {
+					 while ((choice = in.readLine()) == null || choice.length() == 0);
+					 try 
+					 {
+						 Integer.parseInt(choice);
+					 } catch (Exception e) { }
+					 if(isUsersCar(choice))
+					 {
+						 break;
+					 }
+					 System.out.println("Not a valid vin. Try again: ");
+				 }
+			 } catch (IOException e1) {}
+			
+			//update
+			System.out.println("Choose a category: ");
+			while(c2<1 | c2>3)
+			 {
+				 System.out.println("1. Economy");
+				 System.out.println("2. Comfort");
+				 System.out.println("3. Luxury\n");
+				 System.out.println("Choose an option (1-3): ");
+				 
+				 try {
+					 while ((choice = in.readLine()) == null || choice.length() == 0);
+				 } catch (IOException e1) { /*ignore*/}
+				 try{
+					 c2 = Integer.parseInt(choice);
+				 }catch (Exception e)
+				 {
+					 continue;
+				 }
+				 if (c2<1 | c2>3)
+					 continue;
+				 switch(c2) {
+		       	 
+			     	case 1: //Add a new UUber Car
+			     		category = "Economy";
+			       		break;
+			       	case 2: //UUpdate a UUber Car
+			       		category = "Comfort";
+			       		break;
+			       	case 3:
+			       		category = "Luxury";
+			       		break;
+		       	}
+	        }
+			
+			System.out.println("Enter the make of your car: ");
+			while((make = in.readLine()) == null || make.length() == 0);
+			System.out.println("Enter the model of your: ");
+			while((model = in.readLine()) == null || model.length() == 0);
+			System.out.println("Enter the year of your car: ");
+			while((yearS = in.readLine()) == null || yearS.length() == 0) 
+			{
+				try{
+					Integer.parseInt(yearS);
+				 }catch (Exception e)
+				 {
+					 System.out.println("Please enter a valid year");
+				 }
+			}
+			sql = "UPDATE UC SET category = ?, make = ?, model = ?, year = ? where login = ? and vin = ?";
+			try(PreparedStatement pstmt = con.conn.prepareStatement(sql))
+			{
+				pstmt.setString(1, category);
+				pstmt.setString(2, make);
+				pstmt.setString(3, model);
+				pstmt.setString(4, yearS);
+				pstmt.setString(5, userLogin);
+				pstmt.setString(6, choice);
+				int success = pstmt.executeUpdate();
+				if(success == 1)
+				{
+					System.out.println("Car has been updated!\n");
+					return true; // successful update
+				}
+
+			} 
+			catch(SQLException e) 
+			{
+				System.out.println("Car update has failed!\n");
+			}
+			
+		}catch(Exception e) {}
+		
+		return false;
+	}
+	
+	public boolean isUsersCar(String choice)
+	{
+		try 
+		{
+			String sql=null;
+				 
+			sql = "SELECT * FROM UC WHERE vin = ?";
+			try(PreparedStatement pstmt = con.conn.prepareStatement(sql))
+			{
+				pstmt.setString(1,  choice);
+
+				ResultSet result = pstmt.executeQuery();
+				if(result.next())
+				{
+					return true;
+				}
+			} 
+			catch(SQLException e) 
+			{
+				System.out.println("Vehicle chosen is not registered by you");
+			}
+		}
+		catch (Exception e) 
+		{ 
+			System.out.println("Vehicle chosen is not registered by you");
+		}
+		return false; //no car
 	}
 }
