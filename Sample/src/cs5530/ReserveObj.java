d0883a8e4e0de7217de4943348d5ab10b5b0fc50
ukjolie2 <ukjolie2@gmail.com>
2018-03-25 18:05:49 -0600

    Reserve is functional

---
+++ Sample/src/cs5530/ReserveObj.java
@@ -0,0 +1,9 @@
+package cs5530;
+
+public class ReserveObj {
+	public String vin;
+	public int cost;
+	public int start;
+	public int end;
+	public String date;
+}
--- Sample/src/cs5530/UserOptions.java
+++ Sample/src/cs5530/UserOptions.java
@@ -13,11 +13,16 @@ View top awards (class)
 */
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class UserOptions
 {
@@ -45,7 +50,7 @@ public class UserOptions
 			 System.out.println("        Welcome UUber User!     ");
 			 System.out.println("1. Register as a UUber Driver"); //DONE
 			 System.out.println("2. UUber Driver Options"); //DONE / Only works when registered as a UUber Driver
-			 System.out.println("3. Reserve a ride");
+			 System.out.println("3. Reserve a ride"); //DONE
 			 System.out.println("4. Record a ride");
 			 System.out.println("5. Favorite a Car"); //DONE
 			 System.out.println("6. Review a UUber Car"); //DONE
@@ -84,6 +89,7 @@ public class UserOptions
         		 }
         		 break;
         	 case 3: //Reserve a ride
+        		 reserveRide();
         		 break;
         	 case 4: //Record a ride
         		 break;
@@ -540,5 +546,197 @@ public class UserOptions
 		}
 		catch (Exception e) {}
 		return false; //is not own feedback
+	}
+	
+	/*******DATE**********/
+	public static Date parseDate(String date) {
+	     try {
+	         return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+	     } catch (ParseException e) {
+	         return null;
+	     }
+	  }
+	
+	/*******RECORD A RIDE*********/
+	public void recordRide() {
+		
+	}
+	/*******RESERVE A RIDE*********/
+	public void reserveRide()
+	{
+		int c = 0;
+		List<ReserveObj> reservations = new ArrayList<ReserveObj>();
+		System.out.println("        Reserve a Ride!     ");
+		while(c != 2)
+        {
+			 String choice = null;
+			 System.out.println("1. Reserve Ride");
+			 System.out.println("2. Finish"); 
+			 System.out.println("Choose an option (1-2): ");
+			 
+       	 try {
+				while ((choice = in.readLine()) == null || choice.length() == 0);
+			} catch (IOException e1) { /*ignore*/
+			}
+       	 try{
+       		 c = Integer.parseInt(choice);
+       	 }catch (Exception e)
+       	 {
+       		 continue;
+       	 }
+       	 if(c == 1) {
+       		ReserveObj temp = new ReserveObj();
+       		String sql = "SELECT * FROM UC";
+			try(PreparedStatement pstmt = con.conn.prepareStatement(sql))
+			{
+				ResultSet result = pstmt.executeQuery();
+				if(result.isBeforeFirst())
+				{
+					System.out.println("Search results:");
+					while(result.next())
+					{
+						System.out.println("vin: " + result.getString("vin"));
+						System.out.println("\t" + "Category: " + result.getString("category")
+												+ "    Year: " + result.getString("year"));
+					}
+					System.out.println();
+				}
+				else
+				{
+					System.out.println("There are no cars to be reserved");
+					c = 2;
+					continue;
+				}
+			} 
+			catch(SQLException e) {}
+       		System.out.println("Please type in the VIN of the car you'd like to reserve");
+       		while(temp.vin == null) {
+				try
+				{
+					temp.vin = in.readLine();
+				}
+				catch (Exception e) 
+				{
+				}
+       		}
+       		System.out.println("Please type in the date you'd like the reserve a ride (yyyy-mm-dd):");
+       		while(temp.date == null) {
+				try
+				{
+					temp.date = in.readLine();
+					Date test = parseDate(temp.date);
+					if(test == null) {
+						System.out.println("Error: Please type in the date you'd like the reserve a ride (yyyy-mm-dd):");
+						temp.date = null;
+					}
+				}
+				catch (Exception e) 
+				{
+				}
+       		}
+       		System.out.println("Please type in what hour you'd like to reserve the car (only one integer):");
+       		while(temp.start == 0) {
+				try
+				{
+					temp.start = Integer.parseInt(in.readLine());
+				}
+				catch (Exception e) 
+				{
+				}
+       		}
+       		System.out.println("Please type in what hour you'd like your car reservation to end (only one integer):");
+       		while(temp.end == 0) {
+				try
+				{
+					temp.end = Integer.parseInt(in.readLine());
+					int startInt = temp.start;
+					int endInt = temp.end;
+					if(startInt > endInt) {
+						temp.cost = (12 - startInt) + endInt;
+					}
+					else {
+						temp.cost = endInt - startInt;
+					}
+					reservations.add(temp);
+				}
+				catch (Exception e) 
+				{
+				}
+       		}
+       	 }
+       }
+		if(reservations.size() > 0) {
+			int totalCost = 0;
+			String confirmation = "a";
+			System.out.println("Confirm these reservations:");
+			for(int i = 0; i < reservations.size(); i++) {			
+				System.out.println("VIN:" + reservations.get(i).vin);
+				System.out.println("Start Hour:" + String.valueOf(reservations.get(i).start));
+				System.out.println("End Hour:" + String.valueOf(reservations.get(i).end));
+				System.out.println("Date:" + reservations.get(i).date);
+				System.out.println("Cost:" + String.valueOf(reservations.get(i).cost));
+				totalCost = totalCost + reservations.get(i).cost;
+			}
+			System.out.println("Total Cost: " + String.valueOf(totalCost));
+			
+			System.out.println("yes/no?");
+			while(true)
+			{
+				try {
+					confirmation = in.readLine();
+				}
+				catch(Exception e){}
+				if(confirmation.equals(null)) {
+					confirmation = "a";
+				}
+				if(confirmation.equals("yes") || confirmation.equals("no"))
+					break;
+			}
+			if(confirmation.equals("yes")) {
+				for(int i = 0; i < reservations.size(); i++) {
+					String sql = "INSERT INTO Period(fromHour, toHour) VALUES(?,?)";
+					try(PreparedStatement pstmt = con.conn.prepareStatement(sql))
+					{
+						pstmt.setString(1,  String.valueOf(reservations.get(i).start));
+						pstmt.setString(2, String.valueOf(reservations.get(i).end));
+						int success = pstmt.executeUpdate();
+						if(success == 1)
+						{
+							ResultSet generatedKeys = pstmt.getGeneratedKeys();
+							generatedKeys.next();
+							int pid = generatedKeys.getInt(1);
+							sql = "INSERT INTO Reserve(login, vin, pid, cost, date) VALUES(?,?,?,?,?)";
+							try(PreparedStatement pstmt2 = con.conn.prepareStatement(sql))
+							{
+								pstmt2.setString(1,  userLogin);
+								pstmt2.setString(2, reservations.get(i).vin);
+								pstmt2.setString(3,  String.valueOf(pid));
+								pstmt2.setString(4, String.valueOf(reservations.get(i).cost));
+								pstmt2.setString(5,  reservations.get(i).date);
+								pstmt2.executeUpdate();
+							} 
+							catch(SQLException e) 
+							{
+								System.out.println("Failed to insert into Reserve\n");
+								sql = "DELETE FROM Period" + "WHERE pid = " + String.valueOf(pid);
+								try(PreparedStatement pstmt3 = con.conn.prepareStatement(sql))
+								{
+									pstmt3.executeUpdate();
+								} 
+								catch(SQLException eR) 
+								{
+									System.out.println("Failed to remove from Period");
+								}
+							}
+						}
+		
+					} 
+					catch(SQLException e) 
+					{
+						System.out.println("Failed to insert into Period\n");
+					}
+				}
+			}
+		}
 	}
 }
